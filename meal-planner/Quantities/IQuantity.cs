5c29475059de00a7d74104a196977bcdfb822a80
Joseph Wakeling <josephjwakeling@gmail.com>
2021-04-13 20:35:16 +0100

    remove completed TODO

--- meal-planner/Quantities/IQuantity.cs
+++ meal-planner/Quantities/IQuantity.cs
@@ -4,11 +4,6 @@ using System.Collections.Generic;
 
 namespace meal_planner.Quantities
 {
-    //TODO: (worth doing soon) try removing some responsibilities from this interface; it has 4 methods (ignoring overloading)
-    // probably best to split into an ICompoundQuantity and IBaseQuantity{double number, IUnit unit} that exposes both number and unit, but that will require some thought
-    // you probably made this more difficult than it needed to be by trying to resist treating objects like data bags too much
-    // even Bugayenkoism has its limits
-    // on the other hand, maybe you wouldn't have figured out the exact way you needed to split the interfaces without going down this path, who knows?
     public abstract class IQuantity : IParsedJson
     {
         protected abstract string Representation();
